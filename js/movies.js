eed9c045b269a0aa02cb86b0452cab4c3d98d1d2
Joel Ross <joelross@uw.edu>
2016-07-06 19:21:51 -0700

    End of lecture code

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var myApp = angular.module('MoviesApp', []);
+myApp.controller('MoviesCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.message = 'Hello World'; //for testing
+
+  $http.get('data/movies-2015.json').then(function(response){
+      var data = response.data;
+      $scope.movies = data;
+  });
+
+  $scope.ordering = 'gross';
+
+  $scope.sortBy = function(order){
+    //   if($scope.ordering == order){
+    //       $scope.ordering = '-'+order;
+    //   }
+
+      $scope.ordering = order;
+
+
+  };
+
+}]);
--- js/payment.js
+++ js/payment.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var app = angular.module('PaymentApp', []);
+var app = angular.module('PaymentApp', ['credit-cards']);
 app.controller('FormCtrl', ['$scope', function($scope) { 
 
 
 	//a function to call when submitted
   $scope.submitForm = function(form){
-  	if(form.$valid) {
-  		window.alert('Payment info has been submitted!');
-  	}
-  	else {
-  		window.alert('There was an error submitting your payment. Please check that your information is correct.');
-  	}
+		// console.log('$valid', form.username.$valid);
+		// console.log('$error', form.username.$error);
+
+  	// if(form.$valid) {
+  	// 	window.alert('Payment info has been submitted!');
+  	// }
+  	// else {
+  	// 	window.alert('There was an error submitting your payment. Please check that your information is correct.');
+  	// }
   };
   
 }]);
